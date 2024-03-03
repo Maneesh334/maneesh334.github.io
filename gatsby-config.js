e3a083dc4b92063c3c3f58d8f502b85dba777199
Maneesh334 <44678741+Maneesh334@users.noreply.github.com>
2024-03-02 23:43:49 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
 const config = require('./src/config');
 
 module.exports = {
-  pathPrefix: "/Resume2.0",
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Maneesh Reddy',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://maneesh334.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
